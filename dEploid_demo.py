2c113cc4d4b1bab0df06f28f31e71c2ef00a685b
Joe Zhu <sha.joe.zhu@gmail.com>
2018-11-09 13:17:28 +0000

    udpate

--- dEploid_demo.py
+++ dEploid_demo.py
@@ -1,8 +1,6 @@
 import dEploid
 from matplotlib import pyplot
 
-
-
 print("Hello from dEploid")
 print("add: ", dEploid.add(3, 4))
 print(dEploid.getLibraryVertionStr())
@@ -18,7 +16,7 @@ ref = a.get_refCount()
 alt = a.get_altCount()
 
 count_max = max(max(ref), max(alt))
-pyplot.scatter(ref, alt, c = a.get_vqslod())
+pyplot.scatter(ref, alt, c=a.get_vqslod())
 pyplot.xlim(0, 0.9*count_max)
 pyplot.ylim(0, 0.9*count_max)
 pyplot.savefig("altVsRef.png", dpi=72)
